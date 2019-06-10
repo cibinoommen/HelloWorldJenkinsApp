46f56a5783b938efe085a52d231611531086d75d
Krishna Prasad <krishna@smedia.com.au>
2019-06-10 18:19:46 +1000

    initial commit

---
+++ src/main/java/com/cibin/helloworld/HelloWorldJenkinsAppApplication.java
@@ -0,0 +1,13 @@
+package com.cibin.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldJenkinsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldJenkinsAppApplication.class, args);
+	}
+
+}
